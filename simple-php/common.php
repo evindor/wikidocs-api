0b78a3de210211e81e3eba35b58a7f0ac8a6a5fa
Tobias Steiner <ts@girigiri.io>
2013-11-05 15:21:44 +0100

    Factor out common code into common.php

---
+++ simple-php/common.php
@@ -0,0 +1,45 @@
+<?php
+
+// Your Wikidocs app id and app secret
+// 1) Go to https://wikidocs.com/space/create and create
+//    your own space eg. myspace.wikidocs.com
+// 2) Goto to https://myspace.wikidocs.com/admin/space
+//    generate an API key and insert below.
+// 3) You can see the docs you create under
+//    https://myspace.wikidocs.com/me/docs/api
+define("APP_ID", "demo");
+define("APP_SECRET", "demo");
+
+// Allow the document id to be specified in the URL.
+$doc = $_GET['doc'];
+if (!$doc) {
+    $doc = 'simple-demo';
+}
+ 
+$accessData = array(
+    "iss" => "https://wikidocs.com/v1/apps/" . APP_ID,
+    "iat" => time(),
+    "exp" => time() + 60*60,
+    // just to distinguish clients in the demo. Should be the users id ;-)
+    "sub" => $_SERVER['REMOTE_ADDR'], 
+    "access" => array(
+        "/$doc-title"          => "full",
+        "/$doc-teaser"         => "full",
+        "/$doc-content"        => "full"
+    ),
+);
+
+// JSON web token implementation
+// See section "Access Token" at
+// https://wikidocs.com/home/docs/getting-started.html
+// for a list of ready to use libraries.
+function createAccessToken($data, $secret) {
+     $header = encodeBase64UrlSafe(json_encode(array("typ" => "JWT", "alg" => "HS256")));
+     $payload = encodeBase64UrlSafe(json_encode($data));
+     $rawSignature = hash_hmac("sha256", "$header.$payload", $secret, $raw = true);
+     $signature = encodeBase64UrlSafe($rawSignature);
+     return "$header.$payload.$signature";
+}
+function encodeBase64UrlSafe($data) {
+     return strtr(base64_encode($data), "+/", "-_");
+}
--- simple-php/wikidocs-demo.php
+++ simple-php/wikidocs-demo.php
@@ -1,51 +1,5 @@
-<?php
-
-// Your Wikidocs app id and app secret
-// 1) Go to https://wikidocs.com/space/create and create
-//    your own space eg. myspace.wikidocs.com
-// 2) Goto to https://myspace.wikidocs.com/admin/space
-//    generate an API key and insert below.
-// 3) You can see the docs you create under
-//    https://myspace.wikidocs.com/me/docs/api
-define("APP_ID", "demo");
-define("APP_SECRET", "demo");
-
-// get the doc id from URL or use simple-demo as id
-$doc = $_GET['doc'];
-if (!$doc) {
-    $doc = 'simple-demo';
-}
- 
-// The payload of the access token.
-$accessData = array(
-    "iss" => "https://wikidocs.com/v1/apps/" . APP_ID,
-    "iat" => time(),
-    "exp" => time() + 60*60,
-    // just to distinguish clients in the demo. Should be the users id ;-)
-    "sub" => $_SERVER['REMOTE_ADDR'], 
-    "access" => array(
-        "/$doc-title"          => "full",
-        "/$doc-teaser"         => "full",
-        "/$doc-content"        => "full"
-    ),
-);
-
-// JSON web token implementation
-// See https://wikidocs.com/home/docs/getting-started.html
-// section "Access Token" for a list or ready to use libraries
-// for different platforms
-function createAccessToken($data, $secret) {
-     $header = encodeBase64UrlSafe(json_encode(array("typ" => "JWT", "alg" => "HS256")));
-     $payload = encodeBase64UrlSafe(json_encode($data));
-     $rawSignature = hash_hmac("sha256", "$header.$payload", $secret, $raw = true);
-     $signature = encodeBase64UrlSafe($rawSignature);
-     return "$header.$payload.$signature";
-}
-function encodeBase64UrlSafe($data) {
-     return strtr(base64_encode($data), "+/", "-_");
-}
- 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<?php require_once('common.php'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
 <head>
     <title>Wikidocs demo</title>

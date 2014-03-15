b01ffaea254876b486c8a15ac92a06c5dc4ace9e
draftkraft <h.meran@gentics.com>
2014-03-15 19:02:37 +1100

    Added example to compute access token in nodejs

---
+++ nodejs/access-token.js
@@ -0,0 +1,17 @@
+var jwt = require('jwt-simple');
+
+var wdApiId = 'demo',
+	wdApiSecret = 'demo';
+
+var payload = {
+	iss: 'https://wikidocs.com/v1/apps/' + wdApiId,
+	iat: Math.round((new Date().getTime()/1000)),
+	exp: Math.round((new Date().getTime()/1000)) + 3600*24,
+	sub: 'userId-123',
+	access: {}
+};
+payload.access['/content-123'] = 'full';
+
+var accessToken = jwt.encode(payload, wdApiSecret);
+
+console.log(accessToken);

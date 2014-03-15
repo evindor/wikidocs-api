8b82821101b595ebf8ba1c0cd2f6afda6f57e25c
draftkraft <h.meran@gentics.com>
2014-03-15 19:31:32 +1100

    Added ruby example to compute access-token

---
+++ ruby/access-token.rb
@@ -0,0 +1,18 @@
+require 'JWT'
+
+wdApiId = 'demo'
+wdApiSecret = 'demo'
+
+payload = {
+	"iss" => 'https://wikidocs.com/v1/apps/' + wdApiId,
+	"iat" => Time.now.getutc.to_i,
+	"exp" => Time.now.getutc.to_i + 3600*24,
+	"sub" => "userId-123",
+	"access" => {
+		"/content-123" => "full"
+	}
+};
+
+accessToken = JWT.encode(payload, wdApiSecret)
+
+print accessToken, "\n"
